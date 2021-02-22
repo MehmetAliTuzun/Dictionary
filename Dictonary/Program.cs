ac5f0b3a877e770530f4c5f49c07e8123e4288ac
Mehmet Ali TÜZÜN <mehmetali2002tuzun@gmail.com>
2021-02-22 20:16:55 +0300

    Proje dosyası ekle.

---
+++ Dictonary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+
+            students.Add(1284, "Mehmet Ali");
+            students.Add(1263, "Metin");
+            students.Add(1168, "Alp");
+            students.Add(1657, "Recep Ali");
+
+            students.ToList();
+
+
+        }
+    }
+}
